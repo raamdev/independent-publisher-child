03401b7e277d37974549c0e9e75a86e1b18a8a4c
Raam Dev <raam@raamdev.com>
2013-10-02 13:42:57 -0400

    Rearrange sections on archive page

--- content-page-archive.php
+++ content-page-archive.php
@@ -19,39 +19,39 @@ get_header(); ?>
 					<div class="entry-content">
 						<?php the_content(); ?>
 
-						<h2>Search the Archives</h2>
 						<?php get_search_form(); ?>
-
-						<h2>Yearly Archives</h2>
-						<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
-						  <option value=""><?php echo esc_attr( __( 'Select Year' ) ); ?></option> 
-						  <?php wp_get_archives( array( 'type' => 'yearly', 'format' => 'option' ) ); ?>
-						</select>
-
-						<h2>Monthly Archives</h2>
-						<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
-						  <option value=""><?php echo esc_attr( __( 'Select Month' ) ); ?></option> 
-						  <?php wp_get_archives( array( 'type' => 'monthly', 'format' => 'option' ) ); ?>
-						</select>
-
-						<h2>Category Archives</h2>
-						<ul>
-							 <?php wp_list_categories('title_li='); ?>
-						</ul>
 
 						<?php if ( function_exists('wp_tag_cloud') ) : ?>
 							<hr>
 							<div class="commonly-used-tags">
-							<h2>Commonly Used Tags</h2>
+							<h2>Explore by Tag</h2>
 							<?php wp_tag_cloud('smallest=10&largest=22'); ?>
 							</div>
 						<?php endif; ?>
 
-						<?php if ( class_exists('SnazzyArchives') ) : ?> 
+						<?php if ( class_exists('SnazzyArchives') ) : ?>
 							<hr>
-							<h2>Explore the Full Archives (click month headers to see posts)</h2>
+							<h2>The Full Archives</h2>
+							<p>Click on the month headers to see all posts for that month.</p>
 							<?php echo do_shortcode("[snazzy-archive]"); ?>
 						<?php endif; ?>
+
+						<h2>Category Archives</h2>
+						<ul>
+							<?php wp_list_categories('title_li='); ?>
+						</ul>
+
+						<h2>Yearly Archives</h2>
+						<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+							<option value=""><?php echo esc_attr( __( 'Select Year' ) ); ?></option>
+							<?php wp_get_archives( array( 'type' => 'yearly', 'format' => 'option' ) ); ?>
+						</select>
+
+						<h2>Monthly Archives</h2>
+						<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+							<option value=""><?php echo esc_attr( __( 'Select Month' ) ); ?></option>
+							<?php wp_get_archives( array( 'type' => 'monthly', 'format' => 'option' ) ); ?>
+						</select>
 
 						<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'publish' ), 'after' => '</div>' ) ); ?>
 					</div><!-- .entry-content -->
