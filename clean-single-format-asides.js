e2552bc2045485ddcc10396feb42aaa8abf65e2f
Raam Dev <raam@raamdev.com>
2015-05-24 12:55:04 -0400

    Add elements on Single Aside page to be hidden until scrolling

--- clean-single-format-asides.js
+++ clean-single-format-asides.js
@@ -5,9 +5,25 @@ jQuery( function() {
     function pe_check_scroll_position() {
 
         if ( $window.scrollTop() == 0 ) {
+            jQuery( 'body.single-format-aside .entry-header' ).css('opacity', '0');
             jQuery( 'body.single-format-aside .post-author-bottom' ).css('opacity', '0');
+            jQuery( 'body.single-format-aside .entry-meta' ).css('opacity', '0');
+            jQuery( 'body.single-format-aside .comments-area' ).css('opacity', '0');
+            jQuery( 'body.single-format-aside #further-reading' ).css('opacity', '0');
+            jQuery( 'body.single-format-aside #taglist' ).css('opacity', '0');
+            jQuery( 'body.single-format-aside .widget-area' ).css('opacity', '0');
+            jQuery( 'body.single-format-aside .site-footer' ).css('opacity', '0');
+            jQuery( 'body.single-format-aside #respond' ).css('opacity', '0');
         } else {
+            jQuery( 'body.single-format-aside .entry-header' ).css('opacity', '1');
             jQuery( 'body.single-format-aside .post-author-bottom' ).css('opacity', '1');
+            jQuery( 'body.single-format-aside .entry-meta' ).css('opacity', '1');
+            jQuery( 'body.single-format-aside .comments-area' ).css('opacity', '1');
+            jQuery( 'body.single-format-aside #further-reading' ).css('opacity', '1');
+            jQuery( 'body.single-format-aside #taglist' ).css('opacity', '1');
+            jQuery( 'body.single-format-aside .widget-area' ).css('opacity', '1');
+            jQuery( 'body.single-format-aside .site-footer' ).css('opacity', '1');
+            jQuery( 'body.single-format-aside #respond' ).css('opacity', '1');
         }
     }
 
